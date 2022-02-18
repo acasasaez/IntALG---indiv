152399d9e2c8b53c51bd03a5984784f3714469d3
acasasaez <acasasae@myuax.com>
2022-02-18 20:40:52 +0100

    c2

--- Ej9.py
+++ Ej9.py
@@ -9,6 +9,10 @@ Definir MEDIA (n1: REAL, n2: REAL, n3: REAL) REAL #DEFINIMOS LA FUNCIÓN SUMA
 
 #Parte 2 
 Definir MEDIA (p1: REAL, n1: REAL, p2: REAL,n2: REAL, p3: REAL, n3: REAL) REAL
+    precondicion
+        p1 > 0
+        p2> 0
+        p3>0
     suma = p1*n1 + p2*n2 + p3*n3 #Inicialmente necesitamos que nuestra función multiplique cada valor por su peso y los sume 
     media = suma/ (p1 + p2 + p3) #A continuación nuestra función dividirá la suma entre la suma del peso de cada elemento
     resultado = media # Finalmente nuesstra función debe devilvernos el valor de la media ponderada de nuestros tres números
