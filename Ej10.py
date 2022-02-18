695c5af22eb2eb46b50c00d8b84067fef49b884e
acasasaez <acasasae@myuax.com>
2022-02-18 20:46:58 +0100

    respuesta pregunta

--- Ej10.py
+++ Ej10.py
@@ -6,3 +6,6 @@ Definir AREA (b: REAL,h: REAL) REAL #Comenzamos definiendo nuestra función
     mult = b*h #Para esto comienza multiplicando la base por la altura (valores dados)
     area = mult/2 #y finalmente divide el resultado mult entre 2
     resultado = area # para terminar, esta debe devolvernos el resultado del área de nuestro triángulo
+
+#RESPUESTA A LA PREGUNTA
+#Sí, pues tenemos en cuenta que si posicionamos uno de los lados perpendiculares como base el otro corresponde a la altura, de este modo, podemos obtener el área del triángulo rectángulo a partir de la longitud de los lados perpendiculares mediante nuestro algoritmo
